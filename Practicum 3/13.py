99b44476995d4cc825e12a1857b2ff5472b0b9bc
Nikitosishe <billshipher801@gmail.com>
2025-03-03 09:19:00 +0700

    Update 13.py

--- Practicum 3/13.py
+++ Practicum 3/13.py
@@ -1,1 +1,8 @@
-    
+import math
+N = int(input())
+C = int(input())
+number = int(input())
+page_number = 1 + number // (N * C + 1)
+C_number = math.ceil((number % (N * C + 1) + page_number - 1) / N)
+N_number = math.ceil(number % (N * C + 1) + page_number - 1 - N * (C_number - 1))
+print(f'страница {page_number} столбец {С_number} строка {N_number}')
