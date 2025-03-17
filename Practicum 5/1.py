65bf35277b860631129b2242a697bcec8a15cf1b
Nikitosishe <billshipher801@gmail.com>
2025-03-17 14:42:53 +0700

    Update 1.py

--- Practicum 5/1.py
+++ Practicum 5/1.py
@@ -1,5 +1,5 @@
 number_of_days = int(input())
-if number_of_days % 2 == 0:
+if (number_of_days % 4 == 0 and number_of_days % 100 != 0) or number_of_days % 400 == 0:
     print(366)
 else:
     print(365)
