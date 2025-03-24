8c1b57c134a18de3c2fb2a52d093f54583b04066
Nikitosishe <billshipher801@gmail.com>
2025-03-24 14:49:02 +0700

    Update 6.py

--- Practicum 6/6.py
+++ Practicum 6/6.py
@@ -1,3 +1,5 @@
-n, k, m = map(int, input().split())
-answ = (n // k) * 2 * m
-print(answ)
+data = input('Введите данные: ')
+n, k, m = map(int, data.split(' '))
+odin_krug = (n + k - 1) // k
+total_time = odin_krug * 2 * m
+print(total_time)
